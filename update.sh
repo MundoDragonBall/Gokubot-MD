1b0862dac81ad904516c01b2739150d37f59698a
MundoDragonBall <teresacortesoliva1960@gmail.com>
2024-09-29 08:17:03 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
   # * Al bifurcar este repositorio, dejen los créditos a todos los creadores de cada código por favor. ♥
 #!/data/data/com.termux/files/usr/bin/bash 
 BOT_DIR="GokuBot-MD"  
-BOT_REPO="https://github.com/Jostin207/$BOT_DIR"
+BOT_REPO="https://github.com/MundoDragonBall/$BOT_DIR"
 DB_FILE="database.json"
 
 GREEN='\033[32m'
